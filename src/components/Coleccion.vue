c638a0e300a4d227c326604037f25acddf9a3762
PandaHaze <cdjuani.guzmann123@outlook.es>
2023-07-30 01:23:36 -0300

    agregue responsive

--- src/components/Coleccion.vue
+++ src/components/Coleccion.vue
@@ -53,6 +53,15 @@ export default {
     };
   },
   created() {
+    const firebaseConfig = {
+  apiKey: "AIzaSyBKCCiL36TlYdh_-QXxRsJw8zm4z2SCRtw",
+  authDomain: "pokemon-7bc83.firebaseapp.com",
+  projectId: "pokemon-7bc83",
+  storageBucket: "pokemon-7bc83.appspot.com",
+  messagingSenderId: "291971657926",
+  appId: "1:291971657926:web:de2ed95e284bf607c4e7c7"
+};
+    firebase.initializeApp(firebaseConfig);
     this.fetchPokemon();
   },
   watch: {
